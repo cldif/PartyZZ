1f7ff5e2391e7ade39371aeed70f12bf3b48091a
Clément Dif <clement.dif.pro@protonmail.com>
2021-04-09 11:53:05 +0200

    Added Material-UI login page template.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import './App.css';
-import React from 'react';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import RegisterForm from "./routes/RegisterForm";
+import SignIn from "./login";
 import AppHeader from "./components/AppHeader";
-import {FirebaseAuthProvider} from "@react-firebase/auth";
+import { FirebaseAuthProvider } from "@react-firebase/auth";
 import firebase from "firebase/app";
-import {firebaseConfig} from "./index";
+import { firebaseConfig } from "./index";
 
 function App() {
-    return (
-        <FirebaseAuthProvider firebase={firebase} {...firebaseConfig}>
-            <Router>
-                <div className="App">
-                    <AppHeader/>
-                    <Switch>
-                        <Route path={'/register'} component={RegisterForm}/>
-                    </Switch>
-                </div>
-            </Router>
-        </FirebaseAuthProvider>
-    );
+  return (
+    <FirebaseAuthProvider firebase={firebase} {...firebaseConfig}>
+      <Router>
+        <div className="App">
+          <AppHeader />
+          <Switch>
+            <Route path={"/login"} component={SignIn} />
+            <Route path={"/register"} component={RegisterForm} />
+          </Switch>
+        </div>
+      </Router>
+    </FirebaseAuthProvider>
+  );
 }
 
 export default App;
