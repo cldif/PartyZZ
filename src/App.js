a482625747192b65b6ec1eb2b904e6beb5a9091c
Clément Dif <clement.dif.pro@protonmail.com>
2021-04-29 18:35:08 +0200

    Added particles in the background.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import "./App.css";
+import Particles from "react-particles-js";
+import particlesConfig from "./config/particlesConfig";
+
 import React from "react";
 import {
   BrowserRouter as Router,
@@ -30,12 +33,20 @@ export default function App() {
   return (
     <FirebaseAuthProvider firebase={firebase} {...firebaseConfig}>
       <Router>
-        <div className="App">
-          <AppHeader />
-          <Switch>
-            <Route path={"/login"} component={Login} />
-            <Route path={"/register"} component={RegisterForm} />
-          </Switch>
+        <div
+          className="App"
+          style={{ position: "relative", overflow: "hidden" }}
+        >
+          <div style={{ position: "absolute" }}>
+            <Particles height="100vh" width="100vw" params={particlesConfig} />
+          </div>
+          <header className="App-header">
+            <AppHeader />
+            <Switch>
+              <Route path={"/login"} component={Login} />
+              <Route path={"/register"} component={RegisterForm} />
+            </Switch>
+          </header>
         </div>
       </Router>
     </FirebaseAuthProvider>
