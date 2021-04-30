e3a9a34f9b3461bc97f06cfbc3cc7524ddb0a002
Clément Dif <clement.dif.pro@protonmail.com>
2021-04-30 15:09:59 +0200

    :sparkles: Login Page (#4)
    * Added Material-UI login page template.
    
    * Removed the copyright component and added a paper to wrap the form.
    
    * Added a router and a route to access the login page.
    
    * Added Material-UI Lab dependency.
    
    * The Login page is almost finished. (There is still the "success" case to be processed.)
    
    * Implementation of a redirection mechanism after successful login.
    
    * Fixed small mistakes.
    
    * Added particles in the background.
    
    * Improve the state management in the Login component.
    
    * Used AuthConsumer in App.js + Added a paper in RegisterForm.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import './App.css';
-import React from 'react';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import "./App.css";
+import Particles from "react-particles-js";
+import particlesConfig from "./config/particlesConfig";
+
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+  useLocation,
+} from "react-router-dom";
 import RegisterForm from "./routes/RegisterForm";
+import Login from "./routes/login";
 import AppHeader from "./components/AppHeader";
-import {FirebaseAuthProvider} from "@react-firebase/auth";
+import {
+  FirebaseAuthConsumer,
+  FirebaseAuthProvider,
+} from "@react-firebase/auth";
 import firebase from "firebase/app";
-import {firebaseConfig} from "./index";
+import { firebaseConfig } from "./index";
 
-function App() {
-    return (
-        <FirebaseAuthProvider firebase={firebase} {...firebaseConfig}>
-            <Router>
-                <div className="App">
-                    <AppHeader/>
-                    <Switch>
-                        <Route path={'/register'} component={RegisterForm}/>
-                    </Switch>
-                </div>
-            </Router>
-        </FirebaseAuthProvider>
-    );
+const AuthenticatedRoute = (props) => {
+  const location = useLocation();
+
+  return (
+    <FirebaseAuthConsumer>
+      {({ isSignedIn }) => {
+        return isSignedIn ? (
+          <Route {...props} />
+        ) : (
+          <Redirect
+            to={{ pathname: "/login", state: { from: location.pathname } }}
+          />
+        );
+      }}
+    </FirebaseAuthConsumer>
+  );
+};
+
+export default function App() {
+  return (
+    <FirebaseAuthProvider firebase={firebase} {...firebaseConfig}>
+      <Router>
+        <div
+          className="App"
+          style={{ position: "relative", overflow: "hidden" }}
+        >
+          <AppHeader />
+          <div style={{ position: "absolute" }}>
+            <Particles height="100vh" width="100vw" params={particlesConfig} />
+          </div>
+          <header className="App-header">
+            <Switch>
+              <Route path={"/login"} component={Login} />
+              <Route path={"/register"} component={RegisterForm} />
+            </Switch>
+          </header>
+        </div>
+      </Router>
+    </FirebaseAuthProvider>
+  );
 }
-
-export default App;
---
+++ src/routes/login.jsx
@@ -0,0 +1,148 @@
+import React, { useState } from "react";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import Link from "@material-ui/core/Link";
+import MuiAlert from "@material-ui/lab/Alert";
+import Grid from "@material-ui/core/Grid";
+import LocalBarIcon from "@material-ui/icons/LocalBar";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import { Paper, Snackbar } from "@material-ui/core";
+import firebase from "firebase/app";
+import "firebase/auth";
+import { Redirect } from "react-router-dom";
+
+const useStyles = makeStyles((theme) => ({
+  paperContainer: {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -60%)",
+  },
+  paper: {
+    padding: theme.spacing(3),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  loginIcon: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+export default function Login(props) {
+  const classes = useStyles();
+
+  const [successfulLogin, setSuccessfulLogin] = useState(false);
+
+  const [formContent, setFormContent] = useState({
+    email: undefined,
+    password: undefined,
+  });
+
+  const [errorState, setErrorState] = useState({
+    email: false,
+    password: false,
+    displayErrorSnackBar: false,
+  });
+
+  const handleChange = (e) => {
+    const { id, value } = e.target;
+    setFormContent((prevState) => ({ ...prevState, [id]: value }));
+    setErrorState((prevState) => ({ ...prevState, [id]: false }));
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(formContent.email, formContent.password)
+      .then(() => setSuccessfulLogin(true))
+      .catch((_error) => {
+        setErrorState({
+          email: true,
+          password: true,
+          displayErrorSnackBar: true,
+        });
+      });
+  };
+
+  return successfulLogin ? (
+    <Redirect to={props.location.state?.from || "/"} />
+  ) : (
+    <div>
+      <Container className={classes.paperContainer} maxWidth="xs">
+        <Paper elevation={3} className={classes.paper}>
+          <Avatar className={classes.loginIcon}>
+            <LocalBarIcon />
+          </Avatar>
+
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+
+          <form onSubmit={handleSubmit}>
+            <TextField
+              id="email"
+              error={errorState.email}
+              variant="outlined"
+              margin="normal"
+              fullWidth
+              label="Email Address"
+              autoFocus
+              onChange={handleChange}
+            />
+            <TextField
+              id="password"
+              error={errorState.password}
+              variant="outlined"
+              margin="normal"
+              fullWidth
+              label="Password"
+              type="password"
+              autoComplete="current-password"
+              onChange={handleChange}
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+            >
+              Sign In
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="/register" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+          </form>
+        </Paper>
+      </Container>
+
+      <Snackbar
+        open={errorState.displayErrorSnackBar}
+        autoHideDuration={2000}
+        onClose={() =>
+          setErrorState((previous) => ({
+            ...previous,
+            displayErrorSnackBar: false,
+          }))
+        }
+      >
+        <MuiAlert elevation={6} variant="filled" severity="error">
+          Your email and password are not recognized.
+        </MuiAlert>
+      </Snackbar>
+    </div>
+  );
+}
